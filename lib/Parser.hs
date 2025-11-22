17a9458eb67f9db39181129796a68e95c7ba5c84
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2025-11-22 19:47:21 +0100

    Add state

--- lib/Parser.hs
+++ lib/Parser.hs
@@ -7,35 +7,62 @@ module Parser
 where
 
 -- import qualified Data.Text as T
+
+import Control.Monad.State
 import Data.Text (Text)
 import Text.Regex.TDFA
 import Text.Regex.TDFA.Text ()
 
--- true / false
--- if / then / else
--- fun x -> x
--- x
--- f x
+-- TODO:
+-- [x] true / false
+-- [ ] if / then / else
+-- [ ] fun x -> x
+-- [x] x
+-- [ ] f x
 
+type ParserState a = State [Text] a
+
+-- nextToken :: ParserState (Maybe Text)
+-- nextToken = do
+--   tokens <- get
+--   case tokens of
+--     [] -> return Nothing
+--     token : tokens' -> do
+--         put tokens'
+--         return token
+--
+--
 data Term
   = BoolLiteral Bool
   | Var Text
   deriving (Show, Eq)
 
-varRegex::Text
+varRegex :: Text
 varRegex = "[a-zA-Z_][a-zA-Z0-9_]*"
 
 tokenize :: Text -> [Text]
 tokenize program = getAllTextMatches (program =~ ("[a-zA-Z0-9_]+" :: Text))
 
-parseExpression :: [Text] -> Either Text Term
-parseExpression [] = Left "Unexpected end of program"
-parseExpression (token : tokens) =
+nextToken :: ParserState (Maybe Text)
+nextToken = do
+  tokens <- get
+  case tokens of
+    [] -> return Nothing
+    (t : rest) -> do
+      put rest
+      return $ Just t
+
+parseExpression :: ParserState (Either Text Term)
+parseExpression = do
+  token <- nextToken
   case token of
-    "true" -> Right $ BoolLiteral True
-    "false" -> Right $ BoolLiteral False
-    var | var =~ varRegex -> Right $ Var var
-    other -> Left $ "Unexpected token: " <> other
+    Just "true" -> return $ Right $ BoolLiteral True
+    Just "false" -> return $ Right $ BoolLiteral False
+    Just var | var =~ varRegex -> return $ Right $ Var var
+    Just other -> return $ Left $ "Unexpected token: " <> other
+    Nothing -> return $ Left "Unexpected end of program"
 
 parse :: Text -> Either Text Term
-parse program = parseExpression $ tokenize program
+parse program =
+  let tokens = tokenize program
+   in evalState parseExpression tokens
